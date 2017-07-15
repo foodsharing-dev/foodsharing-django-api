d97eb1ad436fb790bc1d3b11d78b2ddd42e9d3f2
Matthias Larisch <mail@matthias-larisch.de>
2017-07-15 23:00:09 +0200

    Optimize store performance

--- foodsharing_api/stores/api.py
+++ foodsharing_api/stores/api.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import GenericViewSet
 
 from foodsharing_api.stores.serializers import StoreSerializer
 from foodsharing_api.stores.models import Store as StoreModel
+from foodsharing_api.utils.decorators import setup_eager_loading
 
 
 class StoreViewSet(
@@ -16,5 +17,6 @@ class StoreViewSet(
     serializer_class = StoreSerializer
     queryset = StoreModel.objects.filter(deleted_at=None)
 
+    @setup_eager_loading
     def get_queryset(self):
-        return self.queryset.filter(team=self.request.user)
+        return self.queryset.filter(team=self.request.user).all().prefetch_related('team')
--- foodsharing_api/stores/serializers.py
+++ foodsharing_api/stores/serializers.py
@@ -2,14 +2,23 @@ from rest_framework import serializers
 
 from foodsharing_api.stores.models import Store as StoreModel
 from foodsharing_api.stores.models import StoreTeam as StoreTeamModel
+from foodsharing_api.users.serializers import UserSerializer
+
 
 class StoreTeamSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = StoreTeamModel
         fields = ['user', 'coordinator']
 
 class StoreSerializer(serializers.ModelSerializer):
-    team = StoreTeamSerializer(many=True)
+    team = StoreTeamSerializer(many=True, source='team_set')
+
+    def setup_eager_loading(queryset):
+        queryset = queryset.prefetch_related('team_set', 'team', 'team_set__user')
+
+        return queryset
+
     class Meta:
         model = StoreModel
-        fields = ['id', 'name', 'street', 'houseNumber', 'zip', 'city', 'notes', 'team_conversation']
+        fields = ['id', 'name', 'street', 'houseNumber', 'zip', 'city', 'notes', 'team_conversation', 'team']
