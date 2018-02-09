e14022b79b2c459d3c9a51082d4bb9b2412277b0
Stefan Farmbauer <s.farmbauer@red-beard-code.de>
2018-02-09 09:36:27 +0100

    Enable SonarQube and clean up user app

--- conftest.py
+++ conftest.py
@@ -11,7 +11,7 @@ from django.conf import settings
 @pytest.fixture(scope='session')
 def django_db_setup():
     """Using the exiting db"""
-    settings.DATABASES['default']= {
+    settings.DATABASES['default'] = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'foodsharing',
         'USER': 'root',
