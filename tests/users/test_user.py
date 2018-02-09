b0b56e0da6037498a938676489fe0472d6e566a3
Stefan Farmbauer <s.farmbauer@red-beard-code.de>
2018-02-09 08:20:46 +0100

    Start to create a Test suite

---
+++ tests/users/test_user.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Testing the user model
+"""
+import pytest
+
+from django.test import TestCase
+
+from foodsharing_api.users.factories import UserFactory
+
+
+class TestUser(TestCase):
+    """Testing the User model"""
+
+    @classmethod
+    def setUpTestData(cls):
+        """Creating a test user"""
+        cls.user = UserFactory.create(
+            first_name='user',
+            last_name='1',
+            email='test@example.com',
+            #TODO: Check if nessecary
+            geschlecht=0,
+            newsletter=0
+        )
+
+    @pytest.mark.django_db
+    def test_creation(self):
+        """Test the creation of an user"""
+        assert self.user.first_name == 'user'
+        assert self.user.last_name == '1'
+        assert self.user.is_active
+        assert self.user.verified
+        assert not self.user.is_staff
+        for field in self.user._meta.local_fields:
+            if field.name not in ['id', 'first_name', 'last_name','verified',
+                                  'email', 'passwd', 'active']:
+                assert not field.value_from_object(self.user)
+
+    def test_password(self):
+        """Testing password handling"""
+        assert self.user.check_password(self.user.email)
+        hashed_pwd = self.user.hash_password(self.user.email)
+        assert hashed_pwd == self.user.passwd
+
+        self.user.set_password('new_password')
+        assert self.user.check_password('new_password')
+
