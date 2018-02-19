a74171bf787ea0dc0b6403d40ebd31467cdb6fcc
Stefan Farmbauer <s.farmbauer@red-beard-code.de>
2018-02-19 18:27:44 +0100

    Linting the pickup app

--- foodsharing_api/pickups/api.py
+++ foodsharing_api/pickups/api.py
@@ -1,22 +1,24 @@
+"""Api for the pickup app"""
+
 import datetime
 
-from django.db.models import Count
 from django.http import Http404
 from rest_framework import mixins
 from rest_framework.decorators import list_route
-from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import BasePermission
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from foodsharing_api.pickups.serializers import TakenPickupSerializer, PickupSerializer
 from foodsharing_api.pickups.models import TakenPickup as TakenPickupModel
-from foodsharing_api.utils.decorators import setup_eager_loading
 
 class IsMember(BasePermission):
+    """Check if user is member of the given store"""
+
     message = ('You are not a member of this store.')
 
     def has_object_permission(self, request, view, obj):
+        """Is the user a registered foodsaver for the store"""
         found = False
         for m in obj.store.team.all():
             if m.id == request.user.id:
@@ -37,7 +39,14 @@ class PickupViewSet(
 
     @list_route(['GET'])
     def next(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.queryset.filter(user=self.request.user).filter(at__gt=datetime.datetime.now() - datetime.timedelta(hours=2)).order_by('at'))
+        """Returns the next planned pickup of a user"""
+        queryset = self.filter_queryset(
+            self.queryset.filter(
+                user=self.request.user
+            ).filter(
+                at__gt=datetime.datetime.now() - datetime.timedelta(hours=2)
+            ).order_by('at')
+        )
 
         page = self.paginate_queryset(queryset)
         if page is not None:
@@ -48,11 +57,13 @@ class PickupViewSet(
         return Response(serializer.data)
 
     def get_serializer_class(self):
+        """Sets the serializer for the pickup"""
         if self.action=='retrieve':
             serializer_class = PickupSerializer
         return serializer_class
 
     def get_object(self):
+        """Returns the pickup for the user for the store"""
         queryset = self.filter_queryset(self.get_queryset())
 
         filter_kwargs = {'store': self.kwargs['store'], 'at': self.kwargs['at']}
@@ -74,6 +85,7 @@ class PickupViewSet(
 
 
     def get_queryset(self):
+        """Returns the predefined QuerySet"""
         if self.action=='retrieve':
             return self.queryset
 
